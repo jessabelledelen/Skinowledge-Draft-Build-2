69042b1ade110df3cb1f7c02b4a8ca2017d44672
jessabelledelen <jessabelle.delen.ramos@gmail.com>
2020-09-22 10:11:31 -0700

    Skinowledge Draft Build 1
    Storyboard progress: Up to Acne Issues
    ViewController progress: First Scene

---
+++ Skinowledge Draft Build 2/ViewController2.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController2.swift
+//  Skinowledge Draft Build 2
+//
+//  Created by Jessabelle Delen Ramos on 9/20/20.
+//
+
+import UIKit
+class ViewController2: UIViewController {
+    
+    @IBAction func AgingIssue(_ sender: Any) {
+        
+    }
+}
